Replaced torch modules by functions in the MLP implementation to fix a backpropagation issue

src/toolbox/modules/object_segmentation_prediction_module.py
@@ -15,10 +15,11 @@ import numpy as np
 # Custom modules
 from toolbox.modules.resnet18_module import ResNet18
 from toolbox.modules.mobile_sam_module import MobileSAM
-from toolbox.modules.segmentation_mask_module import SegmentationMask
 from toolbox.modules.segmentation_with_histograms_module import (
     SegmentationWithHistograms
 )
+from toolbox.modules.probabilistic_segmentation_lookup import ProbabilisticSegmentationLookup
+from toolbox.modules.probabilistic_segmentation_mlp import ProbabilisticSegmentationMLP
 
 
 @dataclass
@@ -44,19 +45,20 @@ class ObjectSegmentationPredictionModel(nn.Module):
     """
     def __init__(
         self,
-        use_histograms: bool = False,
+        probabilistic_segmentation_model: nn.Module,
         compile: bool = False,
     ) -> None:
         """Constructor of the ObjectSegmentationPredictionModel.
 
         Args:
-            use_histograms (bool, optional): Whether to use histograms for
-                implicit object segmentation prediction. Defaults to False.
+            probabilistic_segmentation_model (nn.Module): Model that predicts
+                probabilistic segmentations.
             compile (bool, optional): Whether to compile parts of the model.
                 Defaults to False.
         """
         super().__init__()
         
+        # Set the device
         self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
         
         # Instantiate the MobileSAM module
@@ -66,39 +68,10 @@ class ObjectSegmentationPredictionModel(nn.Module):
             compile=compile,
         )
         
-        if use_histograms:
-            self._segmentation_with_histograms = SegmentationWithHistograms(
-                output_dim=180,  # Hue values
-            )
-            # No need to normalize the RGB images (identity function)
-            self._normalize_transform = lambda x: x
+        self._probabilistic_segmentation_model = probabilistic_segmentation_model
+        self._probabilistic_segmentation_model.device = self._device
         
-        else:
-            # Instantiate the ResNet18 module
-            # (for implicit object segmentation prediction)
-            self._resnet18 = ResNet18(
-                output_dim=180,  # Hue values
-                nb_input_channels=4,  # 3 RGB channels + 1 mask channel
-                inference=True,
-            ).to(device=self._device)
-            self._resnet18.eval()
-            
-            if compile:
-                self._resnet18 =\
-                    torch.compile(self._resnet18)
-            
-            self._normalize_transform = transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],  # Statistics from ImageNet
-                std=[0.229, 0.224, 0.225],
-            )
         
-        # Instantiate the segmentation mask module
-        # (for segmentation mask computation)
-        self._segmentation_mask_module = SegmentationMask()
-        if compile:
-            self._segmentation_mask_module =\
-                torch.compile(self._segmentation_mask_module)
-    
     @torch.no_grad()
     def forward(self, x: BatchInferenceData) -> tuple[torch.Tensor, torch.Tensor]:
         """Forward pass of the model.
@@ -114,46 +87,25 @@ class ObjectSegmentationPredictionModel(nn.Module):
         mobile_sam_outputs = self._mobile_sam(x, x.contour_points_list)
         
         # Stack the masks from the MobileSAM outputs
-        masks = torch.stack([
+        binary_masks = torch.stack([
             output["masks"][:, torch.argmax(output["iou_predictions"])]
             for output in mobile_sam_outputs
         ])
         
-        rgbs = x.rgbs
+        # Get RGB images
+        rgb_images = x.rgbs
         
         # Send images and masks to the device
-        rgbs = rgbs.to(device=self._device)
-        masks = masks.to(device=self._device)
+        rgb_images = rgb_images.to(device=self._device)
+        binary_masks = binary_masks.to(device=self._device)
         
-        #NOTE: from here
-        # Range [0, 255] -> [0, 1]
-        rgbs = rgbs.to(dtype=torch.float32)
-        rgbs /= 255.0
-        
-        # Normalize the RGB images
-        rgbs_normalized = self._normalize_transform(rgbs)
-        
-        # Combine masks and RGB images
-        input_implicit_segmentation = torch.cat([rgbs_normalized, masks], dim=1)
-        
-        # Predict implicit object segmentations using the ResNet18 model or the
-        # SegmentationWithHistograms module (histograms + Bayes)
-        implicit_segmentation_module =\
-            self._resnet18 if hasattr(self, "_resnet18")\
-                else self._segmentation_with_histograms
-        
-        implicit_segmentations = implicit_segmentation_module(
-            input_implicit_segmentation,
+        # Compute the probabilistic segmentation masks
+        probabilistic_masks = self._probabilistic_segmentation_model(
+            rgb_images,
+            binary_masks,
         )
         
-        # Generate the segmentation masks
-        segmentation_masks = self._segmentation_mask_module(
-            rgbs,
-            implicit_segmentations,
-        )
-        #NOTE: to here
-        
-        return segmentation_masks, masks
+        return probabilistic_masks, binary_masks
         
 
 class ObjectSegmentationPredictionModule(nn.Module):
@@ -161,14 +113,24 @@ class ObjectSegmentationPredictionModule(nn.Module):
     Module that predicts object segmentations using the
     ObjectSegmentationPredictionModel.
     """
-    def __init__(self, use_histograms: bool = False, compile: bool = False) -> None:
+    def __init__(
+        self,
+        probabilistic_segmentation_model: nn.Module,
+        compile: bool = False,
+    ) -> None:
         """
         Constructor of the ObjectSegmentationPredictionModule.
+        
+        Args:
+            probabilistic_segmentation_model (nn.Module): Model that predicts
+                probabilistic segmentations.
+            compile (bool, optional): Whether to compile parts of the model.
+                Defaults to False.
         """
         super().__init__()
         
         self._model = ObjectSegmentationPredictionModel(
-            use_histograms=use_histograms,
+            probabilistic_segmentation_model=probabilistic_segmentation_model,
             compile=compile,
         )
     

src/toolbox/modules/pixel_segmentation_mlp_module.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torchinfo
+import torch.nn.functional as F
 
 
 class PixelSegmentationMLP(nn.Module):
@@ -13,7 +14,9 @@ class PixelSegmentationMLP(nn.Module):
         self,
         patch_size: int = 5,
         nb_channels: int = 3,
-        hidden_dims: list[int] = [128, 64, 32],
+        output_size: int = 1,
+        hidden_dims: list[int] = [64, 32, 16],
+        output_logits: bool = True,
     ) -> None:
         """Constructor of the class.
 
@@ -21,81 +24,129 @@ class PixelSegmentationMLP(nn.Module):
             patch_size (int, optional): Side length of the square patch. Defaults to 5.
             nb_channels (int, optional): Number of channels in the input tensor.
                 Defaults to 3.
+            output_size (int, optional): Size of the output tensor. Defaults to 1.
             hidden_dims (list[int], optional): Number of hidden units in each layer of
                 the MLP. Defaults to [128, 64, 32].
+            output_logits (bool, optional): Whether to output logits or probabilities.
+                Defaults to True.
         """
         super(PixelSegmentationMLP, self).__init__()
         
-        # Define the architecture of the network
-        layers = []
-        in_features = patch_size ** 2 * nb_channels
-        for hidden_dim in hidden_dims:
-            layers.append(nn.Linear(in_features, hidden_dim))
-            layers.append(nn.ReLU())
-            in_features = hidden_dim
-        layers.append(nn.Linear(hidden_dims[-1], 1))
-        
-        # Instantiate the network
-        self._mlp = nn.Sequential(*layers)
-        
-        # Freeze the network (paramters will be set externally)
-        # for param in self._mlp.parameters():
-        #     param.requires_grad = False
-        
-        # Compute the total number of parameters
-        self._nb_parameters = sum(p.numel() for p in self._mlp.parameters())
-        
+        self._hidden_dims = hidden_dims
         self._patch_size = patch_size
         self._nb_channels = nb_channels
-    
-    @property
-    def nb_parameters(self) -> int:
-        """Get the total number of parameters in the module.
+        self._output_size = output_size
+        self._output_logits = output_logits
+        
+        self._nb_parameters = self.get_nb_parameters_mlp(
+            input_size=patch_size ** 2 * nb_channels,
+            hidden_dims=hidden_dims,
+            output_size=output_size,
+        )
+        
+    @staticmethod
+    def get_nb_parameters_mlp(
+        input_size: int,
+        hidden_dims: list[int],
+        output_size: int,
+    ) -> int:
+        """Compute the number of parameters in a MLP.
+
+        Args:
+            input_size (int): Size of the input tensor.
+            hidden_dims (list[int]): Number of hidden units in each layer of the MLP.
+            output_size (int): Size of the output tensor.
 
         Returns:
-            int: Total number of parameters.
+            int: Number of parameters in the MLP.
+        """
+        nb_parameters = 0
+        in_features = input_size
+        
+        for hidden_dim in hidden_dims:
+            
+            nb_parameters += in_features * hidden_dim  # Weights
+            nb_parameters += hidden_dim  # Biases
+            
+            in_features = hidden_dim
+        
+        nb_parameters += in_features * output_size  # Final layer weights
+        nb_parameters += output_size  # Final layer biases
+        
+        return nb_parameters
+        
+    @property
+    def nb_parameters(self) -> int:
+        """Return the number of parameters of the model.
+
+        Returns:
+            int: Number of parameters of the model.
         """
         return self._nb_parameters
     
-    def set_model_parameters(self, params: torch.Tensor) -> None:
-        """Set the parameters of the model with the values in the input tensor.
-        
-        Args:
-            params (torch.Tensor): Tensor containing the parameters of the model.
-            
-        Raises:
-            ValueError: If the number of values in the tensor does not match the number
-                of parameters in the model.
-        """
-        # Ensure the tensor has the correct number of values
-        if params.numel() != self._nb_parameters:
-            raise ValueError(
-                "The number of tensor values must match the number of model parameters"
-            )
+    def forward_mlp(self, x: torch.Tensor, parameters: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the MLP. One prediction per patch.
 
+        Args:
+            x (torch.Tensor): Input tensor of shape (B, nb patches, nb features).
+            parameters (torch.Tensor): Parameters of the model.
+
+        Returns:
+            torch.Tensor: Predictions of the module (B, nb patches, output_size).
+        """
         # Ensure the tensor is a 1D tensor
-        params = params.flatten()
+        parameters = parameters.flatten()
         
-        # Assign values from the tensor to model parameters
-        idx = 0
-        for param in self._mlp.parameters():
-            param_numel = param.numel()
-            param.data = params[idx:idx + param_numel].view_as(param).data
-            idx += param_numel
+        in_features = self._patch_size ** 2 * self._nb_channels
+        start_idx = 0
         
-    def forward(self, image_patches: torch.Tensor) -> torch.Tensor:
+        for hidden_dim in self._hidden_dims:
+            end_idx = start_idx + in_features * hidden_dim
+            weight = parameters[start_idx:end_idx].reshape(hidden_dim,
+                                                           in_features)
+            start_idx = end_idx
+            end_idx = start_idx + hidden_dim
+            bias = parameters[start_idx:end_idx]
+            start_idx = end_idx
+
+            x = F.linear(x, weight, bias)
+            x = F.relu(x)
+            in_features = hidden_dim
+        
+        # Final output layer
+        end_idx = start_idx + in_features * self._output_size
+        weight = parameters[start_idx:end_idx].reshape(self._output_size,
+                                                       in_features)
+        start_idx = end_idx
+        bias = parameters[start_idx:start_idx + self._output_size]
+
+        x = F.linear(x, weight, bias)
+        
+        if not self._output_logits:
+            x = torch.sigmoid(x)
+        
+        return x
+
+    def forward(
+        self,
+        image_patches: torch.Tensor,
+        parameters: torch.Tensor,
+    ) -> torch.Tensor:
         """Forward pass of the module.
 
         Args:
             image_patches (torch.Tensor): Batch of image patches
                 (B, nb patches, C, patch_size, patch_size).
+            parameters (torch.Tensor): Parameters of the model.
 
         Raises:
             ValueError: If the input tensor if of incorrect dimension
             ValueError: If the input tensor if of incorrect shape
+            ValueError: If the number of tensor values does not match the number of
+                model parameters
 
         Returns:
-            torch.Tensor: Predictions of the module. 
+            torch.Tensor: Predictions of the module (B, nb patches, output_size).
         """
         # Check that the input tensor is of the correct dimension and shape
         if image_patches.dim() != 5:
@@ -124,8 +175,26 @@ class PixelSegmentationMLP(nn.Module):
             -1,
         )
         
-        return self._mlp(patches_flattened)
+        # Ensure the parameter tensor has the correct number of values
+        if parameters.numel() != self._nb_parameters:
+            raise ValueError(
+                "The number of tensor values must match the number of model parameters"
+            )
+        
+        return self.forward_mlp(patches_flattened, parameters)
 
 
 if __name__ == "__main__":
-    pass
+    
+    # Instantiate the model
+    mlp = PixelSegmentationMLP()
+    
+    # Set the parameters of the model
+    params = torch.randn(mlp.nb_parameters)
+    
+    # Create a random input tensor with appropriate shape
+    input_tensor = torch.randn(2, 10, 3, 5, 5)
+    print("Input shape: ", input_tensor.shape)
+    
+    output = mlp.forward(input_tensor, params)
+    print("Output shape: ", output.shape)

src/toolbox/modules/probabilistic_segmentation_base.py
@@ -25,7 +25,7 @@ class ProbabilisticSegmentationBase(nn.Module, ABC):
             rgb_images (torch.Tensor): Batch of RGB images (B, 3, H, W). Values should
                 be in the range [0, 255] and of type torch.uint8.
             binary_masks (torch.Tensor): Batch of binary masks (B, 1, H, W). Values should
-                be either 0 or 1 and of type torch.float32.
+                be either 0 or 1 and of type torch.float32, or of type torch.bool.
 
         Returns:
             torch.Tensor: Batch of probabilistic segmentation maps (B, H, W). Values
@@ -47,10 +47,10 @@ class ProbabilisticSegmentationBase(nn.Module, ABC):
                 "Input RGB images tensor is of incorrect type. "
                 f"Expected torch.uint8 but got {rgb_images.dtype}."
             )
-        elif binary_masks.dtype != torch.float32:
+        elif binary_masks.dtype != torch.float32 and binary_masks.dtype != torch.bool:
             raise ValueError(
                 "Input binary masks tensor is of incorrect type. "
-                f"Expected torch.float32 but got {binary_masks.dtype}."
+                f"Expected torch.float32 or torch.bool but got {binary_masks.dtype}."
             )
         elif rgb_images.size(1) != 3:
             raise ValueError(

src/toolbox/modules/probabilistic_segmentation_lookup.py
@@ -25,7 +25,7 @@ class ProbabilisticSegmentationLookup(ProbabilisticSegmentationBase):
         compile: bool = False,
         device: Optional[torch.device] = None,
         use_histograms: bool = False,
-        inference: bool = False,
+        output_logits: bool = True,
     ) -> None:
         """Constructor of the class.
 
@@ -36,8 +36,8 @@ class ProbabilisticSegmentationLookup(ProbabilisticSegmentationBase):
                 module. Defaults to None.
             use_histograms (bool, optional): If True, use histograms for implicit
                 object segmentation. Defaults to False.
-            inference (bool, optional): Whether to set the ResNet18 module in inference
-                mode. Defaults to False.
+            output_logits (bool, optional): Whether to output logits or probabilities.
+                Defaults to True.
         """
         super().__init__()
         
@@ -55,11 +55,11 @@ class ProbabilisticSegmentationLookup(ProbabilisticSegmentationBase):
             self._resnet18 = ResNet18(
                 output_dim=self.HUE_VALUES,
                 nb_input_channels=4,  # 3 RGB channels + 1 mask channel
-                inference=inference,
+                output_logits=output_logits,
             ).to(device=device)
             
-            if inference:
-                self._resnet18.eval()
+            # if inference:
+            #     self._resnet18.eval()
             
             if compile:
                 self._resnet18 =\

src/toolbox/modules/probabilistic_segmentation_mlp.py
@@ -1,5 +1,6 @@
 # Standard libraries
 from typing import Optional
+from functools import partial
 
 # Third-party libraries
 import torch
@@ -21,7 +22,7 @@ class ProbabilisticSegmentationMLP(ProbabilisticSegmentationBase):
         patch_size: int = 5,
         compile: bool = False,
         device: Optional[torch.device] = None,
-        inference: bool = False,
+        output_logits: bool = True,
     ) -> None:
         """Constructor of the class.
         
@@ -31,16 +32,18 @@ class ProbabilisticSegmentationMLP(ProbabilisticSegmentationBase):
                 to False.
             device (torch.device, optional): Device on which to run the module. Defaults
                 to None.
-            inference (bool, optional): Whether to set the ResNet18 module in inference
-                mode. Defaults to False.
+            output_logits (bool, optional): Whether to output logits or probabilities.
+                Defaults to True.
         """
         super().__init__()
         
+        # Instantiate the model used to perform pixel-wise segmentation
         self._pixel_segmentation_template = PixelSegmentationMLP(
             patch_size=patch_size,
             nb_channels=3,  # RGB channels
+            output_logits=output_logits,
         ).to(device=device)
-
+        
         # Get the number of parameters of the pixel segmentation model
         nb_parameters_template = self._pixel_segmentation_template.nb_parameters
         
@@ -49,11 +52,11 @@ class ProbabilisticSegmentationMLP(ProbabilisticSegmentationBase):
         self._resnet18 = ResNet18(
             output_dim=nb_parameters_template,
             nb_input_channels=4,  # 3 RGB channels + 1 mask channel
-            inference=inference,
+            output_logits=True,  # True to get the weights and biases of the MLP
         ).to(device=device)
         
-        if inference:
-            self._resnet18.eval()
+        # if inference:
+        #     self._resnet18.eval()
         
         if compile:
             self._resnet18 =\
@@ -202,7 +205,7 @@ class ProbabilisticSegmentationMLP(ProbabilisticSegmentationBase):
         
         # Predict as much weights and biases sets as the number of images in the batch
         pixel_segmentation_parameters = self._resnet18(input_implicit_segmentation)
-
+        
         # Initialize the output masks tensor
         probabilistic_masks = torch.empty(
             rgb_images.shape[0],
@@ -213,17 +216,38 @@ class ProbabilisticSegmentationMLP(ProbabilisticSegmentationBase):
         # TODO: Add some random noise the RGB images to make the segmentation model more robust
         # Compute the probabilistic mask for each image in the batch
         for i in range(rgb_images.shape[0]):
-
+            
             # Set the parameters of the template model with the i-th set of parameters
-            self._pixel_segmentation_template.set_model_parameters(
-                pixel_segmentation_parameters[i]
+            pixel_segmentation_model = partial(
+                self._pixel_segmentation_template,
+                parameters=pixel_segmentation_parameters[i],
             )
-
+            
             # Set the mask for the i-th image in the batch
             probabilistic_masks[i] = self._masks_by_model(
                 rgb_images[i].unsqueeze(0),
-                self._pixel_segmentation_template,
+                pixel_segmentation_model,
                 patch_size=self._patch_size,
             )
-
+        
         return probabilistic_masks
+
+
+if __name__ == "__main__":
+    
+    # Instantiate the model
+    probabilistic_segmentation_model = ProbabilisticSegmentationMLP(
+        patch_size=5,
+        compile=False,
+        device=torch.device("cuda:0"),
+    )
+    
+    # Create a random input tensor with appropriate shape
+    input_tensor = torch.randint(0, 256, (2, 3, 480, 640)).to(dtype=torch.uint8)
+    
+    # Create a random binary mask tensor with appropriate shape
+    mask_tensor = torch.randint(0, 2, (2, 1, 480, 640)).to(dtype=torch.float32)
+    
+    # Forward pass
+    output = probabilistic_segmentation_model(input_tensor, mask_tensor)
+    print("Output shape: ", output.shape)

src/toolbox/modules/resnet18_module.py
@@ -17,7 +17,7 @@ class ResNet18(nn.Module):
         self,
         output_dim: int = 1000,
         nb_input_channels: Optional[int] = None,
-        inference: bool = False,
+        output_logits: bool = True,
     ) -> None:
         """Initialize a pretrained `ResNet18` module.
 
@@ -27,8 +27,8 @@ class ResNet18(nn.Module):
                 If None, the number of input channels is not fixed and can be set at
                 runtime. If not None, the number of input channels is fixed to the
                 specified value. Defaults to None.
-            inference (bool, optional): Whether to set the model in inference mode.
-                Defaults to False.
+            output_logits (bool, optional): Whether to output logits or probabilities.
+                Defaults to True.
         """
         super(ResNet18, self).__init__()
         
@@ -69,10 +69,10 @@ class ResNet18(nn.Module):
         # Apply the sigmoid activation function to the output in inference mode only
         # (not during training because it is applied in the loss function to ensure
         # numerical stability)
-        if inference:
-            self._output_activation = nn.Sigmoid()
-        else:
+        if output_logits:
             self._output_activation = nn.Identity()
+        else:
+            self._output_activation = nn.Sigmoid()
     
     def forward(
         self,